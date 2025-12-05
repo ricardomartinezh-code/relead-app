3781ac51d1aaff81aa49058930b24d5d975f1496
Ricardo Martinez <228397061+ricardomartinezh-code@users.noreply.github.com>
2025-12-05 12:52:36 -0600

    Create post.ts

---
+++ src/app/api/post.ts
@@ -0,0 +1,22 @@
+import { neon } from '@neondatabase/serverless';
+const sql = neon(process.env.DATABASE_URL);
+
+export default async (req: Request, ctx: any) => {
+  // get and validate the `postId` query parameter
+  const postId = parseInt(new URL(req.url).searchParams.get('postId'), 10);
+  if (isNaN(postId)) return new Response('Bad request', { status: 400 });
+
+  // query and validate the post
+  const [post] = await sql`SELECT * FROM posts WHERE id = ${postId}`;
+  if (!post) return new Response('Not found', { status: 404 });
+
+  // return the post as JSON
+  return new Response(JSON.stringify(post), {
+    headers: { 'content-type': 'application/json' }
+  });
+}
+
+export const config = {
+  runtime: 'edge',
+  regions: ['iad1'],  // specify the region nearest your Neon DB
+};
